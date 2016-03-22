def11f59dd6230014b9686701f4b863d4599d1eb
Ian-Woo Kim <ianwookim@gmail.com>
2016-03-22 04:44:10 +0000

    remove unnecessary code.

--- src/Reflex/Polymer.hs
+++ src/Reflex/Polymer.hs
@@ -2,17 +2,9 @@ module Reflex.Polymer ( module Reflex
                   , module Reflex.Polymer.Class
                   , module Reflex.Polymer.Internal
                   , module Reflex.Polymer.Widget
-                  , module Reflex.Polymer.Xhr
-                  , module Reflex.Polymer.Time
-                  , module Reflex.Polymer.WebSocket
-                  , module Reflex.Polymer.Location
                   ) where
 
 import Reflex
 import Reflex.Polymer.Class
 import Reflex.Polymer.Internal
 import Reflex.Polymer.Widget
-import Reflex.Polymer.Xhr
-import Reflex.Polymer.Time
-import Reflex.Polymer.WebSocket
-import Reflex.Polymer.Location
--- src/Reflex/Polymer/Location.hs
+++
@@ -1,3 +0,0 @@
-module Reflex.Polymer.Location (getLocationHost, getLocationProtocol) where
-
-import Reflex.Polymer.Internal.Foreign
